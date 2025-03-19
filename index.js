8a0f4879e7c4259f05e190d65e73c99a4c8b0737
DevBrianKE <kipchumbabrian47@gmail.com>
2025-03-19 06:35:26 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const element = document.getElementById("main")
+ element.remove()
+ 
+ // Create a new <h1> element
+ const newHeader = document.createElement("h1")
+ 
+ // Set the id of the new <h1> element to 'victory'
+ newHeader.id = "victory"
+ 
+ //set the text content of new <h1> element
+ newHeader.textContent = "Kipchumba Brian  N'getich is the champion";
+ 
+ //Append the new <h1> element to the body of the document
+ 
+ document.body.append(newHeader)
